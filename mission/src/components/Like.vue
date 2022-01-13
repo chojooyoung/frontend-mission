0f0c61a0c73a2f5439c766cac7f115669c776297
jooyoung cho <jzo09@naver.com>
2022-01-13 20:27:45 +0900

    ✨ feat: ItemInfo 판매자 정보까지 구현 && LIke 컴포넌트 class통일

--- mission/src/components/Like.vue
+++ mission/src/components/Like.vue
@@ -1,8 +1,8 @@
 <template>
-      <div className ="userLikePost" v-if="this.isLiked=== true">
+      <div class ="userLikePost" v-if="this.isLiked=== true">
         <img  src="../assets/like.svg" alt="" @click="likeButtonClick"/>
       </div>
-      <div className ="userDislikePost" v-else>
+      <div class ="userDislikePost" v-else>
         <img  src="../assets/cancelLike.svg" alt="" @click="likeButtonClick"/>
       </div>
 
--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -1,13 +1,42 @@
 <template>
-<div id='item-info-page'></div>
+<div id='item-info-page'>
+  <div class="info">
+    <img class="info-main_img" :src="`${itemInfo[0].mainImageUrl}`" alt="itemImg"/>
+    <div class="info-seller-wrapper">
+      <!-- 판매자 정보 -->
+      <div class="info-seller">
+        <div v-if="itemInfo[0].author.profileImageUrl === 'null'">
+          <img src="../assets/basicUserAvatar.svg" alt="deafaultUserImg"/>
+        </div>
+        <div v-else>
+          <img class="info-seller-profileImg"
+               :src="`${itemInfo[0].author.profileImageUrl}`"
+               alt="sellerProfileImg"
+          />
+        </div>
+        <div class="info-seller-subinfo">
+          <div class="info-seller-subinfo-name">{{itemInfo[0].author.nickname}}</div>
+          <div class="info-seller-subinfo-tag">{{itemInfo[0].author.tag}}</div>
+        </div>
+        <Like class="like-button" :isLike="itemInfo[0].isLiked" />
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
+import itemInfo from '@/data/itemInfo';
+import Like from '@/components/Like.vue';
+
 export default {
   name: 'ItemInfoPage',
+  components: {
+    Like,
+  },
   data() {
     return {
-
+      itemInfo,
     };
   },
   methods: {
@@ -21,5 +50,44 @@ export default {
 </script>
 
 <style scoped>
+.info{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.info-main_img{
+  width:100%;
+  height:100%;
+  max-width: 375px;
+  max-height: 375px;
 
+}
+.info-seller-wrapper{
+  max-width: 340px;
+  width:100%;
+}
+.info-seller{
+  max-width: 340px;
+  display: flex;
+  width:100%;
+  height:100%;
+  gap: 10px;
+}
+.info-seller-profileImg{
+  border-radius: 50px;
+  width:40px;
+  height:40px;
+}
+
+.info-seller-subinfo-name{
+  text-align: left;
+  font-weight: bold;
+  }
+.info-seller-subinfo-tag{
+  color:#847F7F;
+  font-size: 12px;
+  }
+.like-button{
+  margin-left: auto;
+}
 </style>
