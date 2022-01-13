15448353a291ce50eff7655b2504b311f4aaf1d1
jooyoung cho <jzo09@naver.com>
2022-01-13 18:57:18 +0900

    ✨ feat: 좋아요(즐겨찾기) 컴포넌트 추가

---
+++ mission/src/components/Like.vue
@@ -0,0 +1,33 @@
+<template>
+      <div className ="userLikePost" v-if="this.isLiked=== true">
+        <img  src="../assets/like.svg" alt="" @click="likeButtonClick"/>
+      </div>
+      <div className ="userDislikePost" v-else>
+        <img  src="../assets/cancelLike.svg" alt="" @click="likeButtonClick"/>
+      </div>
+
+</template>
+
+<script>
+export default {
+  name: 'like',
+  props: {
+    isLike: Boolean,
+  },
+  data() {
+    return {
+      isLiked: this.isLike,
+    };
+  },
+
+  methods: {
+    likeButtonClick() {
+      this.isLiked = !this.isLiked;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
