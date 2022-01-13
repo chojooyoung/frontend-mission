2efb8aec9fb07c00578a5ca4e12a76f0858d8e98
jooyoung cho <jzo09@naver.com>
2022-01-13 18:56:57 +0900

    ✨ feat: 상품 이미지, 판매자정보, 좋아요(즐겨찾기)버튼 테스트 unit 추가

--- mission/tests/unit/ItemInfo/ItemInfo.spec.js
+++ mission/tests/unit/ItemInfo/ItemInfo.spec.js
@@ -1,10 +1,28 @@
-import { mount } from '@vue/test-utils';
+import { mount, shallow } from '@vue/test-utils';
 import ItemInfoPage from '@/views/ItemInfo.vue';
+import itemInfo from '@/data/itemInfo';
+import Like from '@/components/Like.vue';
 
 describe('ItemInfoPage', () => {
   it('redners ItemInfoPage', () => {
     const wrapper = mount(ItemInfoPage);
 
     expect(wrapper.find('#item-info-page').exists()).toBe(true);
+  });
+
+  it('has mainimage in ItemInfoPage', () => {
+    const wrapper = mount(ItemInfoPage);
+    expect(wrapper.find('.info-main_img').exists()).toBe(true);
+  });
+
+  it('renders an item-imgae', () => {
+    const wrapper = mount(ItemInfoPage);
+    expect(wrapper.find('.info-main_img').attributes().src).toEqual(itemInfo[0].mainImageUrl);
+  });
+
+  it('change like imgage at a time per like button click', async () => {
+    const likeWrapper = mount(Like);
+    await likeWrapper.find('.userDislikePost').trigger('click');
+    expect(likeWrapper.find('.userDislikePost').exists()).toBe(true);
   });
 });
