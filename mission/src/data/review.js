de06d142c8afa7df48499a4a070af1e46b2282db
jooyoung cho <jzo09@naver.com>
2022-01-13 18:55:34 +0900

    🌱 chore: 더미 데이터 변경 및 유저 기본 프로필 이미지 추가

--- mission/src/data/review.js
+++ mission/src/data/review.js
@@ -20,7 +20,7 @@ const REVIEWS = [
     title: '만족합니다',
     content: '아주 이뻐요',
     createdDate: '2021-12-02T19:19:18',
-    contentImageUrl: 'http://naver.com',
+    contentImageUrl: 'https://user-images.githubusercontent.com/66211721/149164894-aa8dcafe-ad74-4ca1-81e3-0958fdbb964b.jpg',
 
   },
 ];
