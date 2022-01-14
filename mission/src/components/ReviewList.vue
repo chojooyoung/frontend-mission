0e7153637244f77a7e5b0daf1da2ba284faac822
jooyoung cho <jzo09@naver.com>
2022-01-14 18:24:20 +0900

    ✨ feat: ReviewList 컴포넌트 생성

---
+++ mission/src/components/ReviewList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import reviewList from '@/data/review';
+
+export default {
+  name: 'reviewList',
+  props: {
+    postId: String,
+  },
+  data() {
+    return {
+      reviewListData: [{}],
+    };
+  },
+
+  methods: {
+    initBindData() {
+      this.reviewListData = reviewList;
+    },
+  },
+  created() {
+    this.initBindData();
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
