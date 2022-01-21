d9a1030480c3556191dc65b496d5d85993e43b9b
jooyoung cho <jzo09@naver.com>
2022-01-21 18:37:34 +0900

    ✨feat: 페이지에 item컴포넌트 추가

--- mission/src/views/ItemList.vue
+++ mission/src/views/ItemList.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="item-list-page">
     <Header class="item-list-header" :pageName="headerText"/>
+    <div class="item-wrapper">
+      <Item  class="item-list" v-for="item in items" :item="item" :key="item.id"/>
+    </div>
     </div>
 </template>
 
 <script>
 import ItemList from '@/data/itemList';
 import Header from '@/components/ItemList/Header.vue';
+import Item from '@/components/ItemList/Item.vue';
 
 export default {
   name: 'ItemListPage',
   components: {
-    Header,
+    Header, Item,
   },
   data() {
     return {
       itemLists: '',
       headerText: '',
+      items: [],
     };
   },
   methods: {
     initBindData() {
       this.itemLists = ItemList;
       this.headerText = ItemList.pageInfo.pageName;
+      this.items = ItemList.items;
     },
 
   },
@@ -35,7 +41,19 @@ export default {
 </script>
 
 <style>
-#item-list-page{
-  height: 1000px;
+.item-list-page{
+  max-width: 375px;
+  width:100%;
+}
+.item-list-header{
+  text-align: center;
+}
+.item-wrapper{
+  margin-top: 40px;
+}
+.item-wrapper{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 </style>
