05b29eedb990432218f98a358adbc9ab3fa6a182
jooyoung cho <jzo09@naver.com>
2022-01-21 14:34:18 +0900

    ✨ feat: itemList 헤더 컴포넌트 추가

--- mission/src/views/ItemList.vue
+++ mission/src/views/ItemList.vue
@@ -1,12 +1,41 @@
 <template>
-  <div id="item-list-page"></div>
+  <div id="item-list-page">
+    <Header class="item-list-header" :pageName="headerText"/>
+    </div>
 </template>
 
 <script>
+import ItemList from '@/data/itemList';
+import Header from '@/components/ItemList/Header.vue';
+
 export default {
   name: 'ItemListPage',
+  components: {
+    Header,
+  },
+  data() {
+    return {
+      itemLists: '',
+      headerText: '',
+    };
+  },
+  methods: {
+    initBindData() {
+      this.itemLists = ItemList;
+      this.headerText = ItemList.pageInfo.pageName;
+    },
+
+  },
+  computed: {
+  },
+  created() {
+    this.initBindData();
+  },
 };
 </script>
 
 <style>
+#item-list-page{
+  height: 1000px;
+}
 </style>
