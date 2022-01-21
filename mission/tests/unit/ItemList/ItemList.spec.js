8da1b1a76ddc9584c9e07bc26b65265821006de3
jooyoung cho <jzo09@naver.com>
2022-01-22 00:29:15 +0900

    ✅ test: itemList 컴포넌트 TC 추가

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -2,21 +2,27 @@ import { mount } from '@vue/test-utils';
 import ItemListPage from '@/views/ItemList.vue';
 
 describe('ItemListPage', () => {
-  it('redners ItemListPage', async () => {
+  it('renders ItemListPage', async () => {
     const wrapper = mount(ItemListPage);
 
     expect(wrapper.find('#item-list-page').exists()).toBe(true);
   });
 
-  it('redners HeaderComponent', () => {
+  it('renders HeaderComponent', () => {
     const wrapper = mount(ItemListPage);
 
     expect(wrapper.find('.item-list-header').exists()).toBe(true);
   });
 
-  it('redners ItemComponent', () => {
+  it('renders ItemComponent', () => {
     const wrapper = mount(ItemListPage);
 
     expect(wrapper.find('.item-list').exists()).toBe(true);
+  });
+
+  it('renders NavigationComponent', () => {
+    const wrapper = mount(ItemListPage);
+
+    expect(wrapper.find('.navigation-wrapper').exists()).toBe(true);
   });
 });
