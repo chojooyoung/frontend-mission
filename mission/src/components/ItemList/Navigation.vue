a327f3a7f54272effb39ee4cfd807d442b346406
jooyoung cho <jzo09@naver.com>
2022-01-22 00:32:37 +0900

    ✨ feat: Navigation 컴포넌트 로직 추가

---
+++ mission/src/components/ItemList/Navigation.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="navigation-wrapper">
+    <div class="navgation-bar" v-for="navItem in navigationData" :key="navItem.id">
+        <img class="navigation-bar_icon" :src="navItem.navImg"/>
+        {{navItem.navText}}
+    </div>
+</div>
+</template>
+
+<script>
+import reviewList from '@/data/navigation';
+
+export default {
+  name: 'ItemListPage',
+  data() {
+    return {
+      navigationData: [],
+    };
+  },
+  methods: {
+    initBindData() {
+      this.navigationData = reviewList;
+    },
+  },
+  created() {
+    this.initBindData();
+  },
+};
+</script>
+
+<style scoped>
+</style>
