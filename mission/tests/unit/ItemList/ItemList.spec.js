a5cc3fbaa7d6ea60e0fc65d0c38aca4246f83ab8
jooyoung cho <jzo09@naver.com>
2022-02-05 01:54:02 +0900

    ✅ test: api repository 작동테스팅

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -1,23 +1,71 @@
-import { mount } from '@vue/test-utils';
+import { mount, flushPromises } from '@vue/test-utils';
+import itemGet from '@/api/repositories/ItemRepository';
 import ItemListPage from '@/views/ItemList.vue';
+import Item from '@/components/ItemList/Item.vue';
 
-describe('ItemListPage', () => {
-  it('renders ItemListPage', async () => {
-    const wrapper = mount(ItemListPage);
+describe('ItemListPage api testing', () => {
+  let wrapper;
+  let itemWrapper;
 
-    expect(wrapper.find('#item-list-page').exists()).toBe(true);
+  beforeEach(() => {
+    const itemsList = [{
+      description: '아주 잘 맞는 수트',
+      image: 'https://projectlion-vue.s3.ap-northeast-2.amazonaws.com/items/suit-1.png',
+      name: '핏이 좋은 수트',
+      original_price: 298000,
+      price: 198000,
+      product_no: 'asdf1234',
+    }];
+    const res = {
+      data: { items: itemsList },
+    };
+
+    itemGet.get = jest.fn().mockResolvedValue(res);
+    wrapper = mount(ItemListPage);
+    itemWrapper = mount(Item);
+
+    // use mockImplementation
+    // jest.mock('@/api/repositories/ItemRepository', () => jest.fn().mockImplementation(() => ({
+    //   get: jest.fn(() => ({
+    //     data: [{
+    //       description: '아주 잘 맞는 수트',
+    //       image: 'https://projectlion-vue.s3.ap-northeast-2.amazonaws.com/items/suit-1.png',
+    //       name: '핏이 좋은 수트',
+    //       original_price: 298000,
+    //       price: 198000,
+    //       product_no: 'asdf1234',
+    //     }],
+    //   })),
+    // })));
   });
 
+  it('api repository call test', () => {
+    expect(itemGet.get).toHaveBeenCalledTimes(1);
+  });
+
+  it('api repository data test', () => {
+    const itemsList = [{
+      description: '아주 잘 맞는 수트',
+      image: 'https://projectlion-vue.s3.ap-northeast-2.amazonaws.com/items/suit-1.png',
+      name: '핏이 좋은 수트',
+      original_price: 298000,
+      price: 198000,
+      product_no: 'asdf1234',
+    }];
+    const res = {
+      data: { items: itemsList },
+    };
+    itemGet.get().then((data) => {
+      expect(data).toEqual(res);
+    });
+  });
+});
+
+describe('ItemListPage', () => {
   it('renders HeaderComponent', () => {
     const wrapper = mount(ItemListPage);
 
     expect(wrapper.find('.item-list-header').exists()).toBe(true);
-  });
-
-  it('renders ItemComponent', () => {
-    const wrapper = mount(ItemListPage);
-
-    expect(wrapper.find('.item-list').exists()).toBe(true);
   });
 
   it('renders NavigationComponent', () => {
