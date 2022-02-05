fefcdf76c6d6ac295bebcdd9f74d97a45b45e2d4
jooyoung cho <jzo09@naver.com>
2022-02-05 15:40:20 +0900

    ✅ api연동으로 인해 itemInfo TC수정

--- mission/tests/unit/ItemInfo/ItemInfo.spec.js
+++ mission/tests/unit/ItemInfo/ItemInfo.spec.js
@@ -18,14 +18,13 @@ describe('ItemInfoPage', () => {
 
   it('renders and viding data an item-imgae', async () => {
     const wrapper = mount(ItemInfoPage);
-    await wrapper.setData({ mainImageUrl: 'https://user-images.githubusercontent.com/66211721/149165057-fec02ae9-e0a4-4690-93f8-17328de38ab7.png' });
+    await wrapper.setData({
+      itemInfo:
+      {
+        image: 'https://user-images.githubusercontent.com/66211721/149165057-fec02ae9-e0a4-4690-93f8-17328de38ab7.png',
+      },
+    });
     expect(wrapper.find('.info-main_img').attributes().src).toEqual(itemInfo[0].mainImageUrl);
-  });
-
-  it('checks the img style(square)', async () => {
-    const wrapper = mount(ItemInfoPage);
-    await wrapper.setData({ mainImageUrl: 'https://user-images.githubusercontent.com/66211721/149165057-fec02ae9-e0a4-4690-93f8-17328de38ab7.png' });
-    expect(wrapper.find('.info-main_img').element.style).toContain('width');
   });
 
   it('change like imgage at a time per like button click', async () => {
@@ -44,9 +43,11 @@ describe('ItemInfoPage', () => {
   it('renders and viding data an seller-info', async () => {
     const wrapper = mount(ItemInfoPage);
     await wrapper.setData({
-      author: {
-        tag: '#지갑 #잡화',
-        nickname: '지갑전문',
+      item: {
+        seller: {
+          tag: '#지갑 #잡화',
+          nickname: '지갑전문',
+        },
       },
     });
     expect(wrapper.find('.info-seller-subinfo-name').text()).toEqual('지갑전문');
@@ -56,14 +57,13 @@ describe('ItemInfoPage', () => {
   it('renders and viding data an item-info', async () => {
     const wrapper = mount(ItemInfoPage);
     await wrapper.setData({
-      title: '지갑',
-      isDiscount: 'true',
-      price: '100000',
-      discountRate: 30,
-      content: '<div class="content" style="text-align:left; margin-left:20px;">호불호 없는 지갑입니다.</div>',
+      itemInfo: {
+        name: '지갑',
+        price: '100000',
+        description: '<div class="content" style="text-align:left; margin-left:20px;">호불호 없는 지갑입니다.</div>',
+      },
     });
     expect(wrapper.find('.info-item-title').text()).toEqual('지갑');
-    expect(wrapper.find('.info-item-priceinfo-rate').text()).toEqual('30%');
     expect(wrapper.find('.info-item-priceinfo-origin_price').text()).toEqual('100,000 원');
     expect(wrapper.find('.content').text()).toEqual('호불호 없는 지갑입니다.');
   });
@@ -77,10 +77,8 @@ describe('ItemInfoPage', () => {
   it('checks reviewer nickname filter by method', async () => {
     const wrapper = mount(ReviewList);
     await wrapper.setData({
-      id: 1,
-      reviewer: {
-        id: 1,
-        nickname: 'chojoo',
+      reviews: {
+        name: 'chojoo',
       },
     });
     expect(wrapper.find('.reviewer-nickname').text()).toEqual('cho***');
@@ -88,8 +86,7 @@ describe('ItemInfoPage', () => {
   it('checks review time filter by method', async () => {
     const wrapper = mount(ReviewList);
     await wrapper.setData({
-      id: 3,
-      createdDate: '2021-12-02T19:19:18',
+      created: '2021-12-02T19:19:18',
     });
     expect(wrapper.find('.reviewer-time').text()).toEqual('44일전');
   });
